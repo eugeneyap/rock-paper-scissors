d37b5ceee9de5a82337c8c9b605b206d27f49933
Eugene <eugeneyapj@gmail.com>
2022-08-05 18:47:03 +0800

    working game

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
+let playerScore = 0
+let compScore = 0
+
 function getComputerChoice() {
-    const computerOptions = ['rock','paper','scissors']
-    const choiceNumber = Math.floor(Math.random()*3);
-    const computerChoice = computerOptions[choiceNumber];
-    console.log(computerChoice);
-    return computerChoice
+    const arrOfChoices = ['rock', 'paper', 'scissors']
+    const randomNum = Math.floor(Math.random() * 3)
+    return arrOfChoices[randomNum]
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection)
+    if (playerSelection === computerSelection) {
+        return 'Tie!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        compScore++
+        return 'You lost!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++
+        return 'You won!'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore++
+        return 'You lost!'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++
+        return 'You won!'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        compScore++
+        return 'You lost!'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
+        return 'You won!'
+    }
+}
+
+const game = () => {
+    for (let i=0; i<5; i++) {
+        const playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase()
+        const computerSelection = getComputerChoice()
+        console.log(playRound(playerSelection, computerSelection))
+    }
+
+    if (playerScore > compScore) {
+        return 'GG EZ'
+    } else if (playerScore < compScore) {
+        return 'Try again next time'
+    } else {
+        return 'Both players tied'
+    }   
+}
+
+console.log(game())
+console.log(playerScore, compScore)
